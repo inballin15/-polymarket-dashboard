53c16ab023f8c52a84bb3a0e0f752b95497d9cce
inballin15 <inball625@gmail.com>
2026-06-16 09:50:08 -0400

    Add files via upload

---
+++ app_v61_confirm.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="CONFIRM V6.1", page_icon="📈", layout="centered")
+
+st.title("📈 Auto Paper Trader V6.1 CONFIRMATION")
+st.success("This is NOT Version 3. If you see this, the new file is running.")
+
+st.write("After this works, we will replace it with the full mobile trading bot.")
